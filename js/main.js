d31c1af655d89bbec9140f4c78aeb07ca81a90eb
hadanuzi <hadanuzi@gmail.com>
2025-10-15 21:52:00 -0700

    add counter back

--- js/main.js
+++ js/main.js
@@ -13,10 +13,7 @@
     const cfg = {
 
         // Countdown Timer Final Date
-        finalDate : 'December 04, 2025 00:00:00',
-        // MailChimp URL
-        mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
-
+        finalDate : 'May 09, 2026 19:00:00',
     };
 
 
@@ -60,6 +57,60 @@
         });
     };
 
+
+
+   /* Countdown Timer
+    * ------------------------------------------------------ */
+    const ssCountdown = function () {
+
+        const finalDate = new Date(cfg.finalDate).getTime();
+        const daysSpan = document.querySelector('.counter .ss-days');
+        const hoursSpan = document.querySelector('.counter .ss-hours');
+        const minutesSpan = document.querySelector('.counter .ss-minutes');
+        const secondsSpan = document.querySelector('.counter .ss-seconds');
+        let timeInterval;
+
+        if (!(daysSpan && hoursSpan && minutesSpan && secondsSpan)) return;
+
+        function timer() {
+
+            const now = new Date().getTime();
+            let diff = finalDate - now;
+
+            if (diff <= 0) {
+                if (timeInterval) { 
+                    clearInterval(timeInterval);
+                }
+                return;
+            }
+
+            let days = Math.floor( diff/(1000*60*60*24) );
+            let hours = Math.floor( (diff/(1000*60*60)) % 24 );
+            let minutes = Math.floor( (diff/1000/60) % 60 );
+            let seconds = Math.floor( (diff/1000) % 60 );
+
+            if (days <= 99) {
+                if (days <= 9) {
+                    days = '00' + days;
+                } else { 
+                    days = '0' + days;
+                }
+            }
+
+            hours <= 9 ? hours = '0' + hours : hours;
+            minutes <= 9 ? minutes = '0' + minutes : minutes;
+            seconds <= 9 ? seconds = '0' + seconds : seconds;
+
+            daysSpan.textContent = days;
+            hoursSpan.textContent = hours;
+            minutesSpan.textContent = minutes;
+            secondsSpan.textContent = seconds;
+
+        }
+
+        timer();
+        timeInterval = setInterval(timer, 1000);
+    };
 
 
 
@@ -370,6 +421,7 @@
     (function ssInit() {
 
         ssPreloader();
+        ssCountdown();
         ssModal();
         sstabs();
         ssMoveTo();
